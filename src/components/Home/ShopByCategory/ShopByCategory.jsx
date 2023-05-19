9af375221b6e19b9dda54082f3203880d5f347c9
Abrar Saleheen <54772944+saleheen19101331@users.noreply.github.com>
2023-05-19 22:26:54 +0600

    Tab section done in 3 tabs, another 3 sub categories are shown in card format

--- src/components/Home/ShopByCategory/ShopByCategory.jsx
+++ src/components/Home/ShopByCategory/ShopByCategory.jsx
@@ -1,28 +1,113 @@
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 import '../ShopByCategory/ShopByCategory.css';
+import { Rating } from '@smastrom/react-rating'
+
+import '@smastrom/react-rating/style.css'
+import { useEffect, useState } from 'react';
 const ShopByCategory = () => {
+    const [toy, SetToy] = useState([]);
+
+    const allbullet = toy.filter(blt => blt.type == "bullet");
+
+    const allsteam = toy.filter(stm => stm.type == "Steam Engine");
+
+    const alldisney = toy.filter(dsn => dsn.type == "Disney");
+
+    console.log(alldisney);
 
 
-  
+    useEffect(() => {
+        fetch('http://localhost:5000/toy')
+            .then(res => res.json())
+            .then(data => SetToy(data));
+    }, [])
+
+
     return (
         <div className='text-center w-name'>
-            <Tabs  selectedTabClassName = "active">
-                <TabList>
-                    <Tab >Title 1</Tab>
-                    <Tab>Title 2</Tab>
-                    <Tab>Title 3</Tab>
+            <h1 className='p-8 text-5xl'>Toy Categories</h1>
+            <Tabs selectedTabClassName="active mb-10">
+                <TabList className="text-3xl">
+                    <Tab >Bullet</Tab>
+                    <Tab>Steam</Tab>
+                    <Tab>Disney</Tab>
                 </TabList>
 
                 <TabPanel>
-                    <h2>Any content 1</h2>
+                    <div className='grid grid-cols-1 md:grid-cols-3 tabs-content'>
+                        {
+                            allbullet.map(bullet => (
+                                <>
+
+                                    <div className="card card-side bg-base-100 shadow-2xl mr-10 mb-10">
+                                        <figure className='img-color'><img src={bullet.img} alt="Movie" /></figure>
+                                        <div className="card-body bg-sky-400">
+                                            <h2 className="card-title">{bullet.name}</h2>
+                                            <p>Price: $<span>{bullet.price}</span></p>
+                                            <p>Rating: <span>{bullet.rating}</span><span>
+                                            <Rating readOnly = {true} style={{ maxWidth: 250 }} value={bullet.rating} />
+                                                </span></p>
+                                            <div className="card-actions justify-end">
+                                                <button className="btn border-none text-black bg-orange-400">View Details</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </>
+                            ))
+                        }
+                    </div>
                 </TabPanel>
                 <TabPanel>
-                    <h2>Any content 2</h2>
+                    <div className='grid grid-cols-1 md:grid-cols-3 tabs-content'>
+                        {
+                            allsteam.map(bullet => (
+                                <>
+
+                                    <div className="card card-side bg-base-100 shadow-2xl mr-10 mb-10">
+                                        <figure className='img-color'><img src={bullet.img} alt="Movie" /></figure>
+                                        <div className="card-body bg-sky-400">
+                                            <h2 className="card-title">{bullet.name}</h2>
+                                            <p>Price: $<span>{bullet.price}</span></p>
+                                            <p>Rating: <span>{bullet.rating}</span><span>
+                                            <Rating readOnly = {true} style={{ maxWidth: 250 }} value={bullet.rating} />
+                                                </span></p>
+                                            <div className="card-actions justify-end">
+                                                <button className="btn border-none text-black bg-orange-400">View Details</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </>
+                            ))
+                        }
+                    </div>
                 </TabPanel>
                 <TabPanel>
-                    <h2>Any content 3</h2>
+                    <div className='grid grid-cols-1 md:grid-cols-3 tabs-content'>
+                        {
+                            alldisney.map(bullet => (
+                                <>
+
+                                    <div className="card card-side bg-base-100 shadow-2xl mr-10 mb-10">
+                                        <figure className='img-color'><img src={bullet.img} alt="Movie" /></figure>
+                                        <div className="card-body bg-sky-400">
+                                            <h2 className="card-title">{bullet.name}</h2>
+                                            <p>Price: $<span>{bullet.price}</span></p>
+                                            <p>Rating: <span>{bullet.rating}</span><span>
+                                            <Rating readOnly = {true} style={{ maxWidth: 250 }} value={bullet.rating} />
+                                                </span></p>
+                                            <div className="card-actions justify-end">
+                                                <button className="btn border-none text-black bg-orange-400">View Details</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </>
+                            ))
+                        }
+                    </div>
                 </TabPanel>
+
+
             </Tabs>
         </div>
     );
